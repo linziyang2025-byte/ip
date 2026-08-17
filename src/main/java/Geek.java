f812993847eb23654467a642796a0d7cdae5043e
Lin Ziyang <230189112+linziyang2025-byte@users.noreply.github.com>
2026-08-17 20:37:43 +0800

    Implement Level-1 Echo

--- src/main/java/Geek.java
+++ src/main/java/Geek.java
@@ -1,12 +1,26 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Geek {
     public static void main(String[] args) {
         System.out.println("----------------------");
         System.out.println("Hello! I'm Geek.");
         System.out.println("What can I do for you?");
-        System.out.println("----------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("----------------------");
+        System.out.println("----------------------\n");
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("bye")){
+                System.out.println("----------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("----------------------");
+                break;
+            }
+            System.out.println("----------------------");
+            System.out.println(input);
+            System.out.println("----------------------\n");
+        }
+        scanner.close();
     }
 }
