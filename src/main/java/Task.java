ccf5650b08fdcf35f1eea8f8e8a497de8f738730
Lin Ziyang <230189112+linziyang2025-byte@users.noreply.github.com>
2026-08-18 13:50:44 +0800

    Implement Level-4 task types

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,9 +2,21 @@ public class Task {
     private String description;
     private boolean isDone;
 
-    public Task(String description){
+    private Task(String description){
         this.description = description;
         this.isDone = false;
+    }
+
+    public static todoT newTodoT(String input){
+        return new todoT(input);
+    }
+
+    public static ddlT newDdlT(String input, String ddl){
+        return new ddlT(input, ddl);
+    }
+
+    public static eventT newEventT(String input, String startTime, String endTime){
+        return new eventT(input, startTime, endTime);
     }
 
     public void mark(){
@@ -22,6 +34,9 @@ public class Task {
         System.out.println("  " + this);
         System.out.println("----------------------\n");
     }
+    String getDescription(){
+        return description;
+    }
 
     public String getStatus(){
         return isDone ? "X" : " ";
@@ -31,4 +46,44 @@ public class Task {
     public String toString(){
         return String.format("[%s] %s", getStatus(), description);
     }
+
+    private static class todoT extends Task{
+        public todoT(String input){
+            super(input);
+        }
+
+        @Override
+        public String toString(){
+            return String.format("[T][%s] %s", getStatus(), this.getDescription());
+        }
+    }
+
+    private static class ddlT extends Task{
+        private String ddl;
+        public ddlT(String input, String ddl){
+            super(input);
+            this.ddl = ddl;
+        }
+
+        @Override
+        public String toString(){
+            return String.format("[D][%s] %s (by: %s)", getStatus(), this.getDescription(), ddl);
+        }
+    }
+
+    private static class eventT extends Task{
+        private String startTime;
+        private String endTime;
+        public eventT(String input, String startTime, String endTime){
+            super(input);
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        @Override
+        public String toString(){
+            return String.format("[E][%s] %s (from: %s to: %s)", getStatus(), this.getDescription(), startTime, endTime);
+        }
+    }
+
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -17,17 +17,24 @@ public class TaskList {
         } else if (input.equals("list")){
             printTasks();
         } else if(firstFour.equals("mark")){
-            tasks.get(Character.getNumericValue(input.charAt(5)) - 1).mark();
+            int taskNumber = Integer.parseInt(input.substring(5).trim());
+            tasks.get(taskNumber - 1).mark();
         } else if(firstFix.equals("unmark")){
-            tasks.get(Character.getNumericValue(input.charAt(7)) - 1).unmark();
+            int taskNumber = Integer.parseInt(input.substring(7).trim());
+            tasks.get(taskNumber - 1).unmark();
         } else {
             addTask(input);
         }
         return true;
     }
 
+    private int getLen(){
+        return tasks.size();
+    }
+
     private void printTasks(){
         System.out.println("----------------------");
+        System.out.println("Here are the tasks in your list:");
         System.out.println(this);
         System.out.println("----------------------\n");
     }
@@ -38,10 +45,44 @@ public class TaskList {
         System.out.println("----------------------");
     }
     public void addTask(String input){
-        Task newTask = new Task(input);
-        tasks.add(newTask);
+        Task task;
+        if(input.startsWith("todo ")){
+            task = Task.newTodoT(input.substring(5).trim());
+        } else if (input.startsWith("deadline ")){
+            int byIndex = input.indexOf("/by");
+
+            if (byIndex == -1) {
+                System.out.println("Deadline format should be: deadline <description> /by <time>");
+                return;
+            }
+
+            String description = input.substring(9, byIndex).trim();
+            String deadline = input.substring(byIndex + 3).trim();
+
+            task = Task.newDdlT(description, deadline);
+        } else if(input.startsWith("event ")){
+            int fromIndex = input.indexOf("/from");
+            int toIndex = input.indexOf("/to");
+
+            if (fromIndex == -1 || toIndex == -1 || fromIndex >= toIndex) {
+                System.out.println("Event format should be: event <description> /from <start> /to <end>");
+                return;
+            }
+
+            String description = input.substring(6, fromIndex).trim();
+            String from = input.substring(fromIndex + 5, toIndex).trim();
+            String to = input.substring(toIndex + 3).trim();
+
+            task = Task.newEventT(description, from, to);
+        } else {
+            System.out.println("I don't understand :(");
+            return;
+        }
+        tasks.add(task);
         System.out.println("----------------------");
-        System.out.printf("added: %s\n", newTask);
+        System.out.println("Got it. I've added this task:");
+        System.out.printf("  %s\n", task);
+        System.out.printf("Now you have %s tasks in the list.\n", getLen());
         System.out.println("----------------------\n");
     }
 
